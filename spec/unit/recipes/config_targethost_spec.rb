feb36cbbf8beb0e7e57d518dd881ba641dd45f8a
Derek Smart <derek.smart@delphix.com>
2018-05-31 13:19:35 -0400

    fix unit tests
    Signed-off-by: Derek Smart <derek.smart@delphix.com>

--- spec/unit/recipes/config_targethost_spec.rb
+++ spec/unit/recipes/config_targethost_spec.rb
@@ -6,11 +6,14 @@
 
 require 'spec_helper'
 
-describe '.::config_targethost' do
+describe 'delphix::config_targethost' do
+  before(:each) do
+    stub_command("grep -q 'delphix_user ALL=NOPASSWD: /bin/mount, /bin/umount, /bin/ps, /bin/mkdir, /bin/rmdir' /etc/sudoers").and_return(false)
+    stub_command("grep -q 'Defaults:delphix_user !requiretty' /etc/sudoers").and_return(false)
+  end
+
   context 'When all attributes are default, on Ubuntu 16.04' do
     let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
       runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
@@ -22,8 +25,6 @@ describe '.::config_targethost' do
 
   context 'When all attributes are default, on CentOS 7.4.1708' do
     let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
       runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
       runner.converge(described_recipe)
     end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,6 +7,11 @@
 require 'spec_helper'
 
 describe 'delphix::default' do
+  before(:each) do
+    stub_command("grep -q 'delphix_user ALL=NOPASSWD: /bin/mount, /bin/umount, /bin/ps, /bin/mkdir, /bin/rmdir' /etc/sudoers").and_return(false)
+    stub_command("grep -q 'Defaults:delphix_user !requiretty' /etc/sudoers").and_return(false)
+  end
+
   context 'When all attributes are default, on Ubuntu 16.04' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
